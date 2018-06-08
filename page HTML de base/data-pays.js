63a4f15c5aa13ac9e25619b5043ce99c233d0ec0
Megan <megan.kelly@rpn.ch>
2018-06-08 11:37:52 +0200

    page HTML de base

---
+++ page HTML de base/data-pays.js
@@ -0,0 +1,13 @@
+// On définit les choses:
+
+var dataPays = [
+  [ 'Belgique', 6  ],
+  [ 'France',   4  ],
+  [ 'Suisse',   22 ],
+];
+
+var labelsPays = arrayKeys(dataPays);
+var seriesPays = arrayValues(dataPays);
+
+// avec arrayKeys() on obtient les labels
+// avec arrayValues() on obtient les chiffres
