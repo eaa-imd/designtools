5402558f1ca100de17dd3f6f83eee3aebcbf413e
Megan <megan.kelly@rpn.ch>
2018-06-08 11:53:25 +0200

    CSS modification

---
+++ js/base.js
@@ -0,0 +1,11 @@
+$(".icon").click(ain);
+
+function ain() {
+  if ($(".icon .bar").hasClass("active")) {
+    $(".icon .bar").removeClass("active");
+    $(".icon .bar").addClass("notActive");
+  } else {
+    $(".icon .bar").removeClass("notActive");
+    $(".icon .bar").addClass("active");
+  }
+}
