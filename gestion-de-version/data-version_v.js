889fbb6d16e1a529d4f1ed98128ee65f41ef9449
daphneelovas <daphneeb@hotmail.ch>
2018-06-15 11:32:08 +0200

    index_version modification

---
+++ gestion-de-version/data-version_v.js
@@ -0,0 +1,27 @@
+// On définit les choses:
+
+var data = [
+ [ 'github', 10  ],
+  [ 'Google drive',   7  ],
+	[ 'Bitbucket',  2 ],
+	[ 'Dropbox',  2 ],
+	[ 'Abstract',  1 ],
+];
+
+var labelsVersion = arrayKeys(dataPays);
+var seriesVersion = arrayValues(dataPays);
+
+// avec arrayKeys() on obtient les labels
+// avec arrayValues() on obtient les chiffres
+
+
+
+            <h6>
+                    		</br>
+<div class=titre> STREET</div></br></br>
+         		Démonstration footballfreestyle</br></br></br>
+          		Initiation au Football freestyle</br></br></br>
+				tournois de football</br></br></br>
+				tournois de football</br></br></br>
+				Initiation au Football freestyle</br></br></br>
+				tournois de football</h6>
