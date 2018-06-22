18df82177d99b7c62e4b25c9e5841c5777d020af
Manuel Schmalstieg <git.code@ms-studio.net>
2018-06-22 19:15:53 +0200

    Simplification des pages outils
    - On déplace le JavaScript commun dans le fichier graphe.js
    - On garde le tableau des statistiques dans le index.html de chaque page outil.
    - On peut ainsi supprimer plein de fichiers.

--- js/graph.js
+++ js/graph.js
@@ -1,24 +1,68 @@
+
+// Produire le graphe
+
+if (dataArray) {
+
+  var largeur=80;
+        
+  if (document.documentElement.clientWidth >= 600) {
+    var largeur=120;
+  }
+
+  if (document.documentElement.clientWidth >= 900) {
+    var largeur=160;
+  }
+
+  var dataLabels = arrayKeys(dataArray);
+  var dataChiffres = arrayValues(dataArray);
+
+
+  new Chartist.Pie('.ct-chart', {
+     labels: dataLabels,
+     series: dataChiffres
+  }, {
+    donut: true,
+    donutWidth: largeur,
+    donutSolid: true,
+    startAngle: 270,
+    showLabel: true
+  });
+
+  legendeAutomatique(dataArray);
+
+}
+
 // Produire la liste visuelle
 
- function legendeAutomatique(data) {
 
-     var legendeContenu = '';
+function legendeAutomatique(data) {
+
+  var legendeContenu = '';
 
   // Ordonner dataBrain par numéro
+    data.sort(compareSecondColumn);
 
-  for (var i = 0; i < dataBrain.length; i++) {
+    // additionner tous les chiffres
+    var dataChiffres = arrayValues(data);
+    var dataTotal = dataChiffres.reduce((a, b) => a + b, 0);
+
+  for (var i = 0; i < data.length; i++) {
   
-      var legendeData     = dataBrain[i];
-      // var legendeChiffre = seriesBrain[i];
+    var legendeData     = data[i];
+  // var legendeChiffre = seriesBrain[i];
 
-      legendeContenu += '<li>' +legendeData[0]+ ' : ' + legendeData[1] + '</li>';
+    var legendePourcent = Math.round(( legendeData[1] / dataTotal ) * 100);
 
-  }
-
-      var legendeUL = document.createElement("ul");
-      legendeUL.className = "liste-legende";
-      legendeUL.innerHTML = legendeContenu;
-      document.getElementById("legende").appendChild(legendeUL);
-
+    legendeContenu += '<li>' +legendeData[0]+ ' : '+ legendePourcent +'%</li>';
 
   }
+
+  var legendeUL = document.createElement("ul");
+  legendeUL.className = "liste-legende";
+  legendeUL.innerHTML = legendeContenu;
+  document.getElementById("legende").appendChild(legendeUL);
+
+
+}
+
+
