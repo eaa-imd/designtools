9f17b685c76603f156e700ee41ded4e9e2de4df3
Manuel Schmalstieg <git.code@ms-studio.net>
2018-07-07 17:06:10 +0200

    Production en JS du lien vers le chapitre suivant
    On utilise du javascript, comme pour la production du menu.

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+
 // Produire le menu
 
 // on définit la liste des projets (dans un Array javascript).
@@ -5,7 +6,7 @@
 
 function ProduceMenu() {
 
-  var MenuList = [
+  MenuList = [
 
     [ 'Brainstorming', 'brainstorming' ],
     [ 'Wireframing', 'wireframing' ],
@@ -15,7 +16,7 @@ function ProduceMenu() {
     [ 'Édition de code', 'edition-code' ],
     [ 'CMS/frameworks', 'cms-frameworks' ],
 
-    [ 'Gestion de versions et de fichiers', 'gestion-de-version' ],
+    [ 'Gestion de version', 'gestion-de-version' ],
     [ 'Systèmes de design', 'systemes-de-design' ],
     [ 'Communication', 'communication' ]
 
@@ -23,14 +24,13 @@ function ProduceMenu() {
 
   var navProjMenu = '';
 
-  var navPath = '../';
+  navPath = '../';
 
   // Corriger le chemin si on est sur la page d'accueil:
   var navTest = document.querySelectorAll("body.home");
   if (navTest.length == 1) {
     navPath = '';
   }
-
 
   for (var i = 0; i < MenuList.length; i++) {
 
@@ -42,7 +42,25 @@ function ProduceMenu() {
 
   return navProjMenu;
 
+} // end of ProduceMenu()
+
+
+function makeNextPage() {
+
+  var bodyClass = document.querySelector("body").className;
+
+  // tester si la classe figure dans MenuList
+
+  var menuSimple = arrayValues(MenuList);
+
+  var a = menuSimple.indexOf(bodyClass);
+  
+  var nextPage = a + 1;
+
+  return nextPage;
+
 }
+
 
 function InsertMenu() {
 
@@ -70,7 +88,6 @@ function InsertMenu() {
   <li><a href="#">Participants</a></li>
 </ul>`;
 
-
  menuContainer.innerHTML = menuContent;
 
  document.getElementById("menu-icon").addEventListener("click", showHideMenu, false);
@@ -79,6 +96,35 @@ function InsertMenu() {
 
 window.addEventListener("load", InsertMenu, false);
 
+
+function InsertNavNext() {
+
+  var navContainer = document.getElementById("nav-next");
+
+  if (navContainer) {
+
+    var nextPage = makeNextPage();
+
+    if ( nextPage < MenuList.length ) {
+
+      navContent = `
+      <nav class="next">
+        <h2>Suite: </h2>
+        <a href="`+ navPath + MenuList[nextPage][1] +`/index.html">
+          <span class="next-title title-style">`+ MenuList[nextPage][0] +`</span>
+          <img src="`+ navPath +`images/icones/`+ MenuList[nextPage][1] +`.svg" alt="image">
+        </a>
+      </nav>
+      `;
+
+      navContainer.innerHTML = navContent;
+
+    }
+  }
+} // fin de InsertNavNext()
+
+
+window.addEventListener("load", InsertNavNext, false);
 
 function showHideMenu() {
 
